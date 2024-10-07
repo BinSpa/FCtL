66bb00e59d7e5a8ed5880f4ddc7f7b5a2b541539
BinSpa <luogu1024116558@gmail.com>
2024-10-07 15:40:40 +0800

    changes

---
+++ train_bash/gid/train.sh
@@ -0,0 +1,19 @@
+# export CUDA_VISIBLE_DEVICES=0
+../../python train_deep_globe.py \
+--task_name "all" \
+--mode 3 \
+--dataset 'gid' \
+--batch_size 6 \
+--sub_batch_size 8 \
+--size_p 508 \
+--size_g 508 \
+--glo_path_10 "../../saved_models/gid/gidB10.epoch69.pth" \
+--glo_path_15 "../../saved_models/gid/gidB15.epoch24.pth" \
+--pre_path "../../saved_models/gid/gidBfcn.epoch69.pth" \
+--train \
+--val \
+--data_path /data1/gyl/RS_DATASET/FBP \
+--model_path ../../saved_models/gid/ \
+--log_path ../../runs/gid \
+--start 0 \
+--lens 50 \
